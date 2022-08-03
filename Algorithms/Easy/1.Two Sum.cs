45ac2cb0f4f759acfd4382ec53cf75a12f4e463e
ahmed.abozaid1 <ahmed.abozaid128@gmail.com>
2022-08-03 15:21:50 +0200

    enhancement

--- Algorithms/Easy/1.Two Sum.cs
+++ Algorithms/Easy/1.Two Sum.cs
@@ -1,16 +1,21 @@
-public class Solution {
-    public int[] TwoSum(int[] nums, int target) {
-        for(int i=0; i<nums.Length;i++)
+public class Solution
+{    
+    public int[] TwoSum(int[] nums, int target)
+    {
+        for (int i = 0; i < nums.Length; i++)
         {
-            for(int j=i+1;j<nums.Length;j++)
+            for (int j = i + 1; j < nums.Length; j++)
             {
-                if(nums[i]+nums[j]==target)
+                if (nums[i] + nums[j] == target)
                 {
-                    int[] myNum = {i, j};
-                    return myNum;
+                    return new[] { i, j };
                 }
             }
         }
         return null;
     }
 }
+/*
+Runtime: 161 ms, faster than 92.78% of C# online submissions for Two Sum.
+Memory Usage: 44 MB, less than 20.62% of C# online submissions for Two Sum.
+*/
